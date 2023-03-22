75672fe6cb223c133763d92ba33fefcbc5b369c4
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2023-03-22 13:59:46 +0200

    api is working well for questions

--- app.py
+++ app.py
@@ -1,32 +1,68 @@
+import json
+import logging
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 @app.route("/", methods=("GET", "POST"))
+@cross_origin()
 def index():
-    if request.method == "POST":
-        syllabus = request.form["syllabus"]
-        syllabusPoint = request.form["syllabusPoint"]
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = json.loads(request.data)
+        print("data: ", data['syllabus'], data['syllabusPoint'])
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(syllabus, syllabusPoint),
+            prompt=generate_prompt(data['syllabus'], data['syllabusPoint']),
             max_tokens=2048,
             temperature=0.2,
         )
-        print(response)
-        return response
-    return  'Get Request Not Possible'
+        return response.choices[0].text
+    else:
+        return 'Content-Type not supported!'
+    
+    # if request.method == "POST":
+    #     print("This is a POST Method")
+    #     syllabus = request.form["syllabus"]
+    #     syllabusPoint = request.form["syllabusPoint"]
+    #     print("syllabus: ", syllabus, "syllabusPoint: ", syllabusPoint)
+    #     response = openai.Completion.create(
+    #         model="text-davinci-003",
+    #         prompt=generate_prompt(syllabus, syllabusPoint),
+    #         max_tokens=2048,
+    #         temperature=0.2,
+    #     )
+    #     print("response: ", response)
+    #     response.headers.add("Access-Control-Allow-Origin", "*")
+    #     # logging.getLogger('flask_cors').level = logging.DEBUG
+    #     return response
+    # logging.getLogger('flask_cors').level = logging.DEBUG
+    # return  'Get Request Not Possible'
 
 
 def generate_prompt(syllabus, syllabusPoint):
     return """Give me one or more example problems to help me learn:  
-        {syllabusPoint}I'm preparing for {syllabus} without the solution. 
-        The response shouldn't include, "I hope this helps! Let me know if you have any questions".
-        Write any equations in latex. Make solution as detailed as possible. OUTPUT ONLY a javascript object for example {question: 'Solve the equation $2x + 3 = 11$', solution: '$x = 4$'}
-
+        {syllabusPoint} I'm preparing for {syllabus} without the solution.
+        Insert the text breakForAnswer between the problem and solution.
+        Output the solution after the text breakForAnswer.
+        Write any equations in latex. Make solution as detailed as possible. 
         """
+
+@app.route("/test", methods=["POST"])
+@cross_origin()
+def post_example():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print("json: ", json)
+        return json
+    else:
+        return 'Content-Type not supported!'
